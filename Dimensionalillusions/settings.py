4c1e5ffefbe725f3ece63eabe6c1265416abebcb
Sam619 <70689865+Sam231221@users.noreply.github.com>
2021-12-01 13:51:52 +0545

    updated

--- Dimensionalillusions/settings.py
+++ Dimensionalillusions/settings.py
@@ -82,6 +82,14 @@ WSGI_APPLICATION = 'Dimensionalillusions.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+'''
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'DimensionalIllusionV1',  
         'USER': 'postgres',    
@@ -91,22 +99,13 @@ DATABASES = {
     }
 }
 
-
-'''
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 '''
 
 
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 
