f6db03574b3a2f6efe8865277a453617847f921e
Sam619 <70689865+Sam231221@users.noreply.github.com>
2022-04-06 11:31:06 +0545

    Staticfiles Version Updated

--- static/js/popover.928e022c3379.js
+++
@@ -1,5 +0,0 @@
-var popoverTriggerList = [].slice.call( document.querySelectorAll( '[data-bs-toggle="popover"]' ) );
-var popoverList = popoverTriggerList.map( function( popoverTrigger )
-{
-    return new bootstrap.Popover( popoverTrigger );
-} );
