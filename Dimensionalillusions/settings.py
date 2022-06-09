024b310d8fa83f80908f80d92c4bcb1f58852fc9
Sam231221 <rasengankid999@gmail.com>
2022-06-09 22:00:59 +0545

    updated hosts

--- Dimensionalillusions/settings.py
+++ Dimensionalillusions/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = '#3v6h1vd4g0@2$2%1yr*r_7udp75+%lzl7dn+qhm)@*78w*td+'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['dimensional-illusions.herokuapp.com','ec2-13-232-224-9.ap-south-1.compute.amazonaws.com','dimensionalillusions.com','www.dimensionalillusions.com' ,'127.0.0.1',]
+ALLOWED_HOSTS = ['dimensional-illusions.herokuapp.com','ec2-13-232-224-9.ap-south-1.compute.amazonaws.com:8000','dimensionalillusions.com','www.dimensionalillusions.com' ,'127.0.0.1',]
 
 
 # Application definition
